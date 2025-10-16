0f6103ac55ba3ad334df2d27fcd3c2aaaac0c71d
sakshamkaloo <sakshamkaloo@gmail.com>
2025-10-17 00:18:38 +0530

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-const API = 'https://mycalculators-ccwf.onrender.com/api';
+const API = 'https://mycalculators2-ccwf.onrender.com/api';
 
 const Navbar = ({ setPage }) => (
   <nav style={{ marginBottom: '20px' }}>
